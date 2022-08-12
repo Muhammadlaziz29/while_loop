f4cee9627c02694edd959b2986037959a5ae7eef
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-12 13:29:30 +0500

    hello

--- bin/while_loop07.dart
+++ bin/while_loop07.dart
@@ -3,4 +3,26 @@
   Return odd and even numbers up to "a", whichever is smaller
   
 */
-void main() {}
+int func(int a) {
+  int x = 0;
+  int y = 0;
+  int d = 0;
+
+  while (x<a) {
+    x+=1;
+  }
+  if(x%2==0) {
+    y=y+x;
+  }
+  if(x%2==1) {
+    d=d+x;
+  }
+  if(y<d) {
+    return y;
+  }
+  return d;
+}
+
+void main () {
+  print(func(12));
+}
